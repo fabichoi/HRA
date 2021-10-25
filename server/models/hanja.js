6079bdbc5ae8fd97b0e77c33af42b3724129ebd5
fabichoi <fabianus.c@gmail.com>
2021-10-26 01:50:14 +0900

    한자 모델 추가

--- server/models/hanja.js
+++ server/models/hanja.js
@@ -2,5 +2,8 @@ const mongoose = require('mongoose');
 
 const hanjaSchema = new mongoose.Schema({
     id: { type: Number, required: true, unique: true },
-    level: { type: Number, required, true}
+    level: Number,
+    shape: String,
+    mean: String,
+    sound: String
 })
