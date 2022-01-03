fa4054948873a9da9db0dea0ce748199d0c75e97
fabichoi <fabianus.c@gmail.com>
2022-01-03 23:01:51 +0900

    add evaluation.model.js

---
+++ server/models/evaluation.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const evalSchema = new Schema({
+    hanja_id: Number, right_cnt: Number, wrong_cnt: Number
+}, {
+    versionKey: false
+});
+
+evalSchema.statics.findByHanjaId = function (hanja_id) {
+    return this.find({'hanja_id': hanja_id}).exec();
+};
+
+module.exports = mongoose.model("hanja", hanjaSchema);
